19b1345f16ee21be4760b8b330e50478fcc7084a
Alok Kumar <thakuralok490@gmail.com>
2023-07-15 13:01:24 +0530

    modifying

--- calculator.js
+++ calculator.js
@@ -11,13 +11,54 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
   
-  app.post('/', (req, res) => {
+  app.post('/addition', (req, res) => {
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
     var result = num1 + num2;
   
-    res.send('The result of the calculation is ' + result);
+    res.send('The result of the addition is ' + result);
   });
+
+  app.post('/multiply', (req, res) => {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 * num2;
+  
+    res.send('The result of the multiplication is ' + result);
+  });
+
+  app.post('/subtract', (req, res) => {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 - num2;
+  
+    res.send('The result of the Subtraction is ' + result);
+  });
+
+  app.post('/love', (req, res) => {
+    var num1 = (req.body.num1);
+    var num2 = (req.body.num2);
+    
+    var n = Math.random();
+    n = n * 100;
+    n = Math.floor(n);
+     res.send(`<h1> ${num1}  Love  ${num2}  ${n} %</h1>`);
+  
+    
+  });
+
+  app.post('/div', (req, res) => {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+    var result = num1 / num2;
+   if(num2 ===0){
+    res.send('The result of the division is undefined');
+   } else{
+    res.send('The result of the division is '+ result);
+
+   }
+  });
+
   
 app.listen(3000,function(){
     console.log("server started at port 3000");
